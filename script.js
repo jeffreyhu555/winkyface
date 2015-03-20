65d740fa009822527c09065e022c0e286fc23334
jeffreyhu555 <jeffreyhu555@gmail.com>
2015-03-20 15:54:20 -0500

    init commit

---
+++ script.js
@@ -0,0 +1,4 @@
+window.addEventListener('keydown',function(e){if(e.keyIdentifier=='U+0008'||e.keyIdentifier=='Backspace'){window.alert("this is a string")}},true);
+/*window.addEventListener('keydown',function(e){if(e.keyIdentifier=='U+0008'||e.keyIdentifier=='Backspace'){document.getElementById("demo").innerHTML = "string"}},true);*/
+/*window.addEventListener('keydown',function(e),true);
+var {if(e.keyIdentifier=='U+0008'||e.keyIdentifier=='Backspace'){return "this is a string";}}*/
