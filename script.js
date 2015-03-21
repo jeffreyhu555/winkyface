40e9a1a1c6a7aa3fa26ea7e442f1ea2618aee5d1
ecowingflame <lialex444@gmail.com>
2015-03-20 23:11:30 -0500

    I try
    Testing?

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-window.addEventListener('keydown',function(e){if(e.keyIdentifier=='U+0008'||e.keyIdentifier=='Backspace'){window.alert("this is a string")}},true);
-/*window.addEventListener('keydown',function(e){if(e.keyIdentifier=='U+0008'||e.keyIdentifier=='Backspace'){document.getElementById("demo").innerHTML = "string"}},true);*/
-/*window.addEventListener('keydown',function(e),true);
-var {if(e.keyIdentifier=='U+0008'||e.keyIdentifier=='Backspace'){return "this is a string";}}*/
+function insertAtCursor(myField, myValue) {
+    //IE support
+    if (document.selection) {
+        myField.focus();
+        sel = document.selection.createRange();
+        sel.text = myValue;
+    }
+    //MOZILLA and others
+    else if (myField.selectionStart || myField.selectionStart == '0') {
+        var startPos = myField.selectionStart;
+        var endPos = myField.selectionEnd;
+        myField.value = myField.value.substring(0, startPos)
+            + myValue
+            + myField.value.substring(endPos, myField.value.length);
+        myField.selectionStart = startPos + myValue.length;
+        myField.selectionEnd = startPos + myValue.length;
+    } else {
+        myField.value += myValue;
+    }
+}
+
+$('textarea').bind('keydown',function (e) {
+    var $t = $(this);
+    if (e.which==8){
+    insertAtCursor(this, ';) ');
+    }
+});
+
+$('input').bind('keydown',function (e) {
+    var $t = $(this);
+    if(e.which==8){
+    insertAtCursor(this, ';) ');
+    }
+});
